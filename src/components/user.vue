db77edb17e6e25c15ba8fc5b21e90436a1d894fb
王 磊 <wanglei1@travelsky.com>
2018-01-05 16:40:11 +0800

    动态路由

--- src/components/user.vue
+++ src/components/user.vue
@@ -1,26 +1,36 @@
 <template>
-	<div>
-		<router-link v-for="item,index in data">
+  <div>
+    我是user
+    <div>
+      <router-link style="margin-left: 20px;" :to="'/user'+item.id" :key="index" v-for="(item,index) in dataList">
+        {{item.name}}
+      </router-link>
+    </div>
 
-    </router-link>
 
-	</div>
+  </div>
 </template>
 <style rel="stylesheet/scss" lang="scss" scoped>
-	body {
-	}
+  body {
+  }
 </style>
 <script>
 
-	export default {
-		data() {
-			return {};
-		},
-		components: {},
-		mixins: [],
-		methods: {},
-		mounted() {
-		}
-	};
+  export default {
+    data() {
+      return {
+        dataList: [
+          {id: 1, name: "wang", age: 12},
+          {id: 2, name: "li", age: 33},
+          {id: 3, name: "zhang", age: 55}
+        ]
+      };
+    },
+    components: {},
+    mixins: [],
+    methods: {},
+    mounted() {
+    }
+  };
 </script>
 
